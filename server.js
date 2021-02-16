395672a1c021c79305f56d89d8c3f800eebf7134
Peter Ihimire <peterihimire@gmail.com>
2021-02-16 18:51:47 +0100

    Added mysql2 npm package to connect with mysql database, created a database connection file for the project

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const db = require("./util/database");
 
 const usersRoutes = require("./routes/users-routes");
 
 const HttpError = require("./models/http-error");
 
 const app = express();
+
+db.execute("SELECT * FROM users")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 
---
+++ util/database.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "room-reservation",
+  password: "Eromosele2121991@",
+});
+module.exports = pool.promise();
