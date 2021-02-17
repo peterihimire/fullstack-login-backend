c85cdf0fd9e0083ead0fe11ed62a7838d0e0670a
Peter Ihimire <peterihimire@gmail.com>
2021-02-17 22:22:14 +0100

    Added new property model , property-controller and property-routes, so as to start working on the property API

---
+++ models/property.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+const sequelize = require("../util/database");
+
+const Property = sequelize.define("property", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  location: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  amount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  completion: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  detail: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  images: {
+    type: Sequelize.STRING,
+  },
+});
+
+module.exports = Property;
